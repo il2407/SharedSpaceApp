4727b6a1278e36955d6889d8e543048a97c888b8
il2407 <idolev53@gmail.com>
2023-08-03 12:28:07 +0300

    commit

--- Desktop/ShardSpace APP/Project/client/App.js
+++ Desktop/ShardSpace APP/Project/client/App.js
@@ -2,17 +2,13 @@ import React, { useState, useEffect } from "react";
 import { LogBox, SafeAreaView } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import AuthStack from "./src/Navigation/AuthStack";
-import { NavigationContainer } from '@react-navigation/native';
-
-
+import { NavigationContainer } from "@react-navigation/native";
 
 const App = () => {
-
   return (
     <NavigationContainer>
-          <AuthStack />
+      <AuthStack />
     </NavigationContainer>
-
   );
 };
 export default App;
